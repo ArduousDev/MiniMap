b8daccd5b4a8502a5d798776725fcadd200fe62a
Lorenzo <cocoetmathieudu86@gmail.com>
2020-01-27 16:23:15 +0100

    optimizations ??? why do loop it

--- client.lua
+++ client.lua
@@ -1,10 +1,3 @@
 Citizen.CreateThread(function()
-    while true do
-		Citizen.Wait(1)
-		if IsPedOnFoot(GetPlayerPed(-1)) then 
-			SetRadarZoom(1100)
-		elseif IsPedInAnyVehicle(GetPlayerPed(-1), true) then
-			SetRadarZoom(1100)
-		end
-    end
+    SetRadarZoom(1100)
 end)
